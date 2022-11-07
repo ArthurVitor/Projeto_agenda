f911f84801294252de5527bff6610bef416f1a3b
Arthur <arthurvitorgn@gmail.com>
2022-11-06 22:17:09 -0300

    End

--- contatos/views.py
+++ contatos/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .models import  Contato
+from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 # Create your views here.
-
+valor = 0
 def index(request):
-    contatos = Contato.objects.all()
-    paginator = Paginator(contatos, 5)
+    contatos = Contato.objects.order_by('id').filter(
+        mostrar = True
+    )
 
-    page = request.GET.get('p')
-    contatos = paginator.get_page(page)
 
     return render(request, 'contatos/index.html', {
         'contatos': contatos
@@ -18,6 +19,26 @@ def index(request):
 def ver_contato(request, contato_id):
     #contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:
+        raise Http404
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo = campos
+    ).filter(
+        Q(nome_completo__icontains = termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos':contatos
     })
